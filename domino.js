7b9bb5bdbbd55106954212865fc90f5b896d0932
jackliu612 <jackliu612@gmail.com>
2020-11-20 23:11:28 -0500

    minor editing changes

--- domino.js
+++ domino.js
@@ -1,72 +1,75 @@
-function domino(x1,y1, angle1){
+function Domino(x1, y1, angle1) {
     var x = x1
     var y = y1
     var angle = angle1
-    var toppled = false 
+    var toppled = false
     var neighbors = new Set()
 
-    this.draw = function(shadow, col){
+    this.draw = function (shadow, col) {
         push();
-        if(col)
-            fill(128,160,82)
+        if (col)
+            fill(128, 160, 82)
         else
-            fill(180,180,180)
-        translate(x,y)
+            fill(180, 180, 180)
+        translate(x, y)
         rotate(angle)
-        if(this.toppled == true){
-            rect(0,0,100, 80)
-        } 
-        else{
-            rect(0,0,100, 20)
+        if (this.toppled === true) {
+            rect(0, 0, 100, 80)
+        } else {
+            rect(0, 0, 100, 20)
 
-            if(shadow){
-                fill(180,180,180,40)
-                rect(0,0,100,80)
+            if (shadow) {
+                fill(180, 180, 180, 40)
+                rect(0, 0, 100, 80)
             }
         }
         pop();
     };
 
-    this.topple = function(){
+    this.topple = function () {
         this.toppled = true
     };
 
-    this.getX = function(){
+    this.getX = function () {
         return x;
     };
 
-    this.getY = function(){
+    this.getY = function () {
         return y;
     }
 
-    this.getAngle = function(){
+    this.getAngle = function () {
         return angle;
     }
 
-    this.move = function(x,y, angle){
-        this.x = x 
-        this.y = y 
+    this.move = function (x, y, angle) {
+        this.x = x
+        this.y = y
         this.angle = angle
-        this.draw()
-        push()
-        fill(180,180,180)
-        translate(x,y)
-        rotate(angle)
-
-        rect(0,0,100, 20)
-
-        fill(180,180,180,40)
-        rect(0,0,100,80)
-        pop(); 
+        this.draw(false, false);
+        // push()
+        // fill(180, 180, 180)
+        // translate(x, y)
+        // rotate(angle)
+        //
+        // rect(0, 0, 100, 20)
+        //
+        // fill(180, 180, 180, 40)
+        // rect(0, 0, 100, 80)
+        // pop();
     }
 
-    this.addNeighbor = function(n){
-        if (this != n)
+    this.addNeighbor = function (n) {
+        if (this !== n)
             neighbors.add(n)
     }
 
-    this.getNeighbors = function(){
+    this.getNeighbors = function () {
         return neighbors
+    }
+
+    this.reset = function () {
+        this.toppled = false;
     }
 
 }
--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-function setup(){
+function setup() {
     createCanvas(windowWidth * 0.9, windowHeight);
     scale(0.05)
     background(11);
@@ -6,44 +6,42 @@ function setup(){
     cursor(CROSS)
 
 }
+
 domino_x = 100
 domino_y = 20
 domino_shadow = 80
 angle = 0
 
-hover = (new domino(0,0,angle))
-dominos = [] 
+hover = (new Domino(0, -domino_y, angle))
+dominos = []
 
 next = new Set()
 active = false
 
-function draw(){
+function draw() {
     background(11)
-    if(active){
+    if (active) {
         frameRate(16)
-        if(next.size > 0){
-            next.forEach(function(d){
+        if (next.size > 0) {
+            next.forEach(function (d) {
                 topple(d)
             })
-        }
-        else{
-            active = false 
+        } else {
+            active = false
             frameRate(60)
         }
+    } else {
+        hover.move(mouseX - cos(angle), mouseY - sin(angle), angle)
     }
-    else{
-        hover.move(mouseX - cos(angle),mouseY - sin(angle), angle)
-    }
-    for(var i = dominos.length - 1; i >= 0; i--){
-        if(i == dominos.length - 1)
+    for (var i = dominos.length - 1; i >= 0; i--) {
+        if (i === dominos.length - 1)
             dominos[i].draw(true)
-        if(hover_collision(i)){
+        if (hover_collision(i)) {
             dominos[i].draw(false, true)
-        }
-        else
+        } else
             dominos[i].draw()
-        if(collision(i, dominos.length - 1)){
-            dominos[i].addNeighbor(dominos[dominos.length-1])
+        if (collision(i, dominos.length - 1)) {
+            dominos[i].addNeighbor(dominos[dominos.length - 1])
 
         }
     }
@@ -52,67 +50,80 @@ function draw(){
 
 
 function mouseClicked() {
-    dominos.push(new domino(mouseX, mouseY, angle))
+    dominos.push(new Domino(mouseX, mouseY, angle))
 }
 
 document.addEventListener('keydown', function (event) {
     if (event.key === 'z') {
-        angle -= PI/10
+        angle -= PI / 10
     }
     if (event.key === 'x') {
-        angle += PI/10
+        angle += PI / 10
     }
 
-    if (event.key === "T"){
-            dominos.pop()
+    if (event.key === "T") {
+        dominos.pop()
     }
 
-    if (event.key === "V"){
+    if (event.key === "V") {
         activate()
+    }
+
+    if (event.key === 'r') {
+        reset();
     }
 });
 
-function hover_collision(n){
+function hover_collision(n) {
     r1x = dominos[n].getX()
     r1y = dominos[n].getY()
-    r2x = mouseX 
+    r2x = mouseX
     r2y = mouseY
-    r1w = 100, r2w = 100
-    r1h= 80, r2h = 80
-    return(r1x + r1w >= r2x && // r1 right edge past r2 left
+    r2w = 100
+    r1w = 100
+    r1h = 80
+    r2h = 80
+    return (r1x + r1w >= r2x && // r1 right edge past r2 left
         r1x <= r2x + r2w && // r1 left edge past r2 right
         r1y + r1h >= r2y && // r1 top edge past r2 bottom
         r1y <= r2y + r2h // r1 bottom edge past r2 top)
     )
 }
 
-function collision(a,b){
+function collision(a, b) {
     r1x = dominos[a].getX()
     r1y = dominos[a].getY()
     r2x = dominos[b].getX()
     r2y = dominos[b].getY()
-    r1w = 100, r2w = 100
-    r1h= 80, r2h = 80
-    return(r1x + r1w >= r2x && // r1 right edge past r2 left
+    r1w = 100
+    r2w = 100
+    r1h = 80
+    r2h = 80
+    return (r1x + r1w >= r2x && // r1 right edge past r2 left
         r1x <= r2x + r2w && // r1 left edge past r2 right
         r1y + r1h >= r2y && // r1 top edge past r2 bottom
         r1y <= r2y + r2h // r1 bottom edge past r2 top)
     )
 }
 
-function activate(){
-      active = true
-      topple(dominos[0])
+function activate() {
+    active = true
+    topple(dominos[0])
 }
 
-function topple(d){
+function topple(d) {
     d.topple()
     next.delete(d)
     neighbors = d.getNeighbors()
     print(neighbors.size)
-    neighbors.forEach(function(n){
+    neighbors.forEach(function (n) {
         next.add(n)
     })
     print(next)
 }
 
+function reset() {
+    dominos.forEach(function (d) {
+        d.reset();
+    })
+}
